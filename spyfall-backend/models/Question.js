381f7b47401aa033294f98ed30bdf41f5dae70de
MuhammadOusman <ousmansohail786@gmail.com>
2025-07-31 04:02:37 +0500

    here we go again

--- spyfall-backend/models/Question.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  options: [{ type: String, required: true }],
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Question', questionSchema);
